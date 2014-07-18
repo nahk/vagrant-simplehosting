f76841361982dadecd9775cd0329ec16cd905776
nahk <chery.clement@gmail.com>
2014-07-18 17:06:58 +0200

    Bootstrap: add anacron

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y apache2 mysql-client mysql-server php5-fpm php5-mysql php-apc php5-intl php5-xdebug phpmyadmin
+apt-get install -y apache2 mysql-client mysql-server php5-fpm php5-mysql php-apc php5-intl php5-xdebug phpmyadmin anacron
 rm -rf /var/www
 ln -fs /vagrant /var/www
