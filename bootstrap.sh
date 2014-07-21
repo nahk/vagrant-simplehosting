8407a1b5e789a61c71875e1bc91f6bcf726a285d
Tibbo <leclercq.thibault@yahoo.fr>
2014-07-21 19:02:30 +0200

    Updated sources.list

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Installing sources.list..."
+rm -f "/etc/apt/sources.list"
+wget -O "/etc/apt/sources.list" "https://raw.githubusercontent.com/CestanGroupeNumerique/vagrant-simplehosting/master/sources.list"
+
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install debian-archive-keyring
 DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes upgrade
@@ -15,6 +19,7 @@ debconf-set-selections <<< 'phpmyadmin phpmyadmin/app-password-confirm password 
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/setup-password password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
+debconf-set-selections <<< 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2'
 
 DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install apache2 mysql-client mysql-server php5-common libapache2-mod-php5 php5-cli php5-json php5-mysql php5-mcrypt php5-curl php5-gd php5-xsl php5-xmlrpc php-apc php5-intl php5-xdebug phpmyadmin anacron git
 rm -rf /var/www
@@ -25,4 +30,5 @@ wget -O "/home/vagrant/.bash_aliases" "https://raw.githubusercontent.com/CestanG
 echo "Installing php settings..." 
 wget -O "/etc/php5/mods-available/php-custom.ini" "https://raw.githubusercontent.com/CestanGroupeNumerique/vagrant-simplehosting/master/php-custom.ini"
 
+a2enmod rewrite
 service apache2 restart
