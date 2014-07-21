dd24bc58f6de73b61b992077447298f2ad0cdb89
Tibbo <leclercq.thibault@yahoo.fr>
2014-07-21 17:30:30 +0200

    well

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,11 +16,13 @@ debconf-set-selections <<< 'phpmyadmin phpmyadmin/setup-password password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/admin-pass password root'
 debconf-set-selections <<< 'phpmyadmin phpmyadmin/mysql/app-pass password root'
 
-DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install apache2 mysql-client mysql-server php5-common libapache2-mod-php5 php5-cli php5-mysql php5-mcrypt php5-curl php5-gd php5-xsl php5-xmlrpc php-apc php5-intl php5-xdebug phpmyadmin anacron git
+DEBIAN_FRONTEND=noninteractive apt-get --yes --force-yes install apache2 mysql-client mysql-server php5-common libapache2-mod-php5 php5-cli php5-json php5-mysql php5-mcrypt php5-curl php5-gd php5-xsl php5-xmlrpc php-apc php5-intl php5-xdebug phpmyadmin anacron git
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
-wget -q  -O "~/.bash_aliases" "https://raw.githubusercontent.com/CestanGroupeNumerique/vagrant-simplehosting/master/bash_aliases"
-wget -q  -O "/etc/php5/mods-available/php-custom.ini" "https://raw.githubusercontent.com/CestanGroupeNumerique/vagrant-simplehosting/master/php-custom.ini"
+echo "Installing bash aliases..." 
+wget -O "~/.bash_aliases" "https://raw.githubusercontent.com/CestanGroupeNumerique/vagrant-simplehosting/master/bash_aliases"
+echo "Installing php settings..." 
+wget -O "/etc/php5/mods-available/php-custom.ini" "https://raw.githubusercontent.com/CestanGroupeNumerique/vagrant-simplehosting/master/php-custom.ini"
 
 service apache2 restart
